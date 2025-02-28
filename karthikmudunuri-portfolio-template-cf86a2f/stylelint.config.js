00d23c2566faeccabc4e7fa8ec5b89570e9818fd
mohamedAtoui <mohamedelamineatoui@gmail.com>
2025-02-28 23:10:44 +0000

    fix/gg

--- karthikmudunuri-portfolio-template-cf86a2f/stylelint.config.js
+++ karthikmudunuri-portfolio-template-cf86a2f/stylelint.config.js
@@ -1,3 +1,4 @@
+/*
 module.exports = {
   extends: ["stylelint-config-standard", "stylelint-config-prettier"],
   rules: {
@@ -12,6 +13,33 @@ module.exports = {
     ],
     "block-no-empty": null,
     "unit-no-unknown": [true, { ignoreUnits: ["em", "rem", "s"] }],
+  },
+  ignoreFiles: ["node_modules/**", ".next"],
+};
+*/
+
+module.exports = {
+  extends: ["stylelint-config-standard", "stylelint-config-prettier"],
+  rules: {
+    "declaration-empty-line-before": null,
+    "function-name-case": null, // Disabled
+    "value-keyword-case": null, // Disabled
+    "at-rule-no-unknown": [
+      false, // Less strict
+      {
+        ignoreAtRules: ["extends", "apply", "variants", "tailwind"],
+      },
+    ],
+    "block-no-empty": null,
+    "unit-no-unknown": [
+      false, // Less strict
+      {
+        ignoreUnits: ["em", "rem", "s", "vh", "vw", "ch", "%"],
+      },
+    ],
+    "selector-class-pattern": null, // Allow any class naming
+    "no-descending-specificity": null, // Avoid specificity warnings
+    "max-nesting-depth": null, // No nesting limits
   },
   ignoreFiles: ["node_modules/**", ".next"],
 };
