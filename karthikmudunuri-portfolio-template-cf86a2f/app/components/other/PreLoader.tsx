5708f8231b59fd4981ee9c387d15b2107ebae447
mohamedAtoui <mohamedelamineatoui@gmail.com>
2025-03-01 11:19:23 +0000

    minor/changes

--- karthikmudunuri-portfolio-template-cf86a2f/app/components/other/PreLoader.tsx
+++ karthikmudunuri-portfolio-template-cf86a2f/app/components/other/PreLoader.tsx
@@ -1,78 +1,35 @@
 import React, { useEffect } from "react";
 import gsap from "gsap";
-import Logo from "../svg/Logo";
 
 const PreLoader: React.FC = () => {
   useEffect(() => {
     const tl = gsap.timeline();
 
-    // Preloader Animation
-    const preLoaderAnim = () => {
-      tl.to(".texts-container", {
-        duration: 0,
-        opacity: 1,
+    tl.to(".texts-container", {
+      duration: 0,
+      opacity: 1,
+      ease: "Power3.easeOut",
+    })
+      .from(".texts-container span", {
+        duration: 0.8,
+        y: 70,
+        skewY: 10,
         ease: "Power3.easeOut",
       })
-        .from(".texts-container span", {
-          duration: 1.5,
-          delay: 1,
-          y: 70,
-          skewY: 10,
-          stagger: 0.4,
-          ease: "Power3.easeOut",
-        })
-        .to(".texts-container span", {
-          duration: 1,
-          y: 70,
-          skewY: -20,
-          stagger: 0.2,
-          ease: "Power3.easeOut",
-        })
-        .to("body", {
-          duration: 0.01,
-          css: { overflowY: "scroll" },
-          ease: "power3.inOut",
-        })
-        .from(".sub", {
-          duration: 1,
-          opacity: 0,
-          y: 80,
-          ease: "expo.easeOut",
-        })
-        .to(
-          ".preloader",
-          {
-            duration: 1.5,
-            height: "0vh",
-            ease: "Power3.easeOut",
-            onComplete: mobileLanding,
-          },
-          "-=2"
-        )
-        .to(".preloader", {
-          duration: 0,
-          css: { display: "none" },
-        });
-    };
-
-    const mobileLanding = () => {
-      if (window.innerWidth < 763) {
-        tl.from(".landing__main2", {
-          duration: 1,
-          delay: 0,
-          opacity: 0,
-          y: 80,
-          ease: "expo.easeOut",
-        });
-      }
-    };
-
-    preLoaderAnim();
-  }, []); // Empty dependency array to run the animation only on mount
+      .to(".preloader", {
+        duration: 1,
+        height: "0vh",
+        ease: "Power3.easeOut",
+      })
+      .to(".preloader", {
+        duration: 0,
+        css: { display: "none" },
+      });
+  }, []);
 
   return (
     <div
-      className="preloader gap-[5px] overflow-hidden text-[14px] sm:gap-[10px] sm:text-[16px] md:text-[18px] lg:text-[20px]"
+      className="preloader"
       style={{
         height: "100vh",
         width: "100%",
@@ -86,23 +43,16 @@ const PreLoader: React.FC = () => {
         display: "flex",
         justifyContent: "center",
         alignItems: "center",
-        overflow: "hidden !important",
       }}
     >
       <div
-        className="texts-container w-500 flex h-60 items-center justify-center gap-[5px] overflow-hidden text-[14px] font-bold text-[#e4ded7] opacity-0 sm:gap-[10px] sm:text-[16px] md:text-[18px] lg:text-[20px]"
+        className="texts-container"
         style={{
           height: "60px",
+          opacity: 0,
         }}
       >
-        <span>Web Developer</span>
-        <span> / </span>
-
-        <span className="flex items-center justify-center gap-3">
-          {" "}
-          Building <Logo width={30} height={30} /> Eldora UI
-        </span>
-        <div className="sub hidden"></div>
+        <span className="text-xl font-bold">Welcome</span>
       </div>
     </div>
   );
